a036a31ad812d387eb2086834b52894322280c49
Hugo Freire <hfreire@ntrglobal.com>
2012-02-21 10:57:20 +0100

    send headers

--- cli.rb
+++ cli.rb
@@ -7,7 +7,6 @@ Starting pub/sub ... Use --help to list all options
 EOS
 
 @options = Trollop::options do
-
   opt :host, "Host for the amqp server", :default => "127.0.0.1"
   opt :user, "User for the amqp connection", :default => "guest"
   opt :password, "Password for the amqp connection", :default => "guest"
@@ -17,9 +16,11 @@ EOS
   opt :routing_key, "Routing key for the message sended", :default => "testroutingkey"
   opt :message, "Message to send to exchange", :default => "test message"
   opt :queue, "Queue name", :default => "testqueue"
+  opt :headers, "Extra headers", :default => "{}"
 end
 
 @options = OpenStruct.new(@options)
+@options.headers = eval(@options.headers)
 
 def self.cli
   @options
--- send.rb
+++ send.rb
@@ -5,7 +5,7 @@ require 'amqp'
 require_relative 'amqp.rb'
 
 run do |connection, channel, exchange, options|
-  exchange.publish(options.message, :routing_key => options.routing_key, :timestamp => Time.now.to_i) do
+  exchange.publish(options.message, :routing_key => options.routing_key, :timestamp => Time.now.to_i, :headers => options.headers) do
     puts "message sended: #{options.message}"
     connection.disconnect { EventMachine.stop }
   end
